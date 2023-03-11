40a2298c8755f6f197e8ba8ebd04c526a64bf769
qseudo <57642143+qseudo@users.noreply.github.com>
2023-03-11 16:39:00 +0900

    completes contacts app

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 const { body, validationResult } = require('express-validator');
+const session = require('express-session');
+const store = require('connect-loki');
+const flash = require('express-flash');
 
-let contactData = [
+const app = express();
+const LokiStore = store(session);
+
+const contactData = [
   {
     firstName: "Mike",
     lastName: "Jones",
@@ -42,7 +47,7 @@ const sortContacts = contactsArr => {
   });
 };
 
-let validateName = (field, whichName) => {
+const validateName = (field, whichName) => {
   return body(field)
     .trim()
     .isLength({ min: 1, max: 25 })
@@ -52,12 +57,44 @@ let validateName = (field, whichName) => {
     .withMessage(`${whichName} name must be only alphabetical characters.`);
 };
 
+const clone = object => {
+  return JSON.parse(JSON.stringify(object));
+};
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('common'));
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    maxAge: 31 * 24 * 60 * 60 * 1000,
+    path: '/',
+    secure: false,
+  },
+  name: 'contacts-manager-session-id',
+  resave: false,
+  saveUninitialized: true,
+  secret: 'not-very-secure',
+  store: new LokiStore({}),
+}));
+
+app.use((req, res, next) => {
+  if (!('contactData' in req.session)) {
+    req.session.contactData = clone(contactData);
+  }
+
+  next();
+});
+
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
 
 app.get('/', (req, res) => {
   res.redirect('/contacts');
@@ -65,7 +102,7 @@ app.get('/', (req, res) => {
 
 app.get('/contacts', (req, res) => {
   res.render("contacts", {
-    contacts: sortContacts(contactData),
+    contacts: sortContacts(req.session.contactData),
   });
 });
 
@@ -88,16 +125,24 @@ app.post('/contacts/new',
   (req, res, next) => {
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
+      errors.array().forEach(error => req.flash('error', error.msg));
       res.render('new-contact', {
-        errorMessages: errors.array().map(err => err.msg),
-        ...req.body,
+        flash: req.flash(),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber,
       });
     } else {
       next();
     }
   },
   (req, res) => {
-    contactData.push({ ...req.body });
+    req.session.contactData.push({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber,
+    });
+    req.flash('success', 'New contact added to list!');
     res.redirect('/contacts');
   },
 );
